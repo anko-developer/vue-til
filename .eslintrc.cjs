0b52c6f470a7389a54db24f9ad09857dff62f357
김명욱 <anko@gsitm.com>
2023-02-27 18:36:17 +0900

    setting

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+	root: true,
+	extends: [
+		'plugin:vue/vue3-essential',
+		'eslint:recommended',
+		'@vue/eslint-config-prettier',
+	],
+	env: {
+		node: true,
+	},
+	parserOptions: {
+		ecmaVersion: 2022,
+		sourceType: 'module',
+	},
+	rules: {
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+			},
+		],
+	},
+};
