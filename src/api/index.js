1f6ce5079e9d240ba683fd9a02f917a3fa974484
김명욱 <anko@gsitm.com>
2023-03-06 09:23:33 +0900

    axios instance

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,17 @@
 import axios from 'axios';
 
+const instance = axios.create({
+	// baseURL: 'http://localhost:3000/',
+	baseURL: import.meta.env.VITE_APP_API_URL,
+});
+
 function registerUser(userData) {
-	const url = 'http://localhost:3000/signup';
-	return axios.post(url, userData);
+	// const url = 'http://localhost:3000/signup';
+	// return axios.post('http://localhost:3000/signup', userData);
+	return instance.post('signup', userData);
 }
-export { registerUser };
+
+function loginUser(userData) {
+	return instance.post('login', userData);
+}
+export { registerUser, loginUser };
