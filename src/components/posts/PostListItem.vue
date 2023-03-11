ec6e6401f1bdb3281d81b8d4ab50002e675bc4a7
김명욱 <anko@gsitm.com>
2023-03-11 10:25:36 +0900

    postItem 컴포넌트화

---
+++ src/components/posts/PostListItem.vue
@@ -0,0 +1,18 @@
+<template>
+	<li>
+		<div class="post-title">{{ item.title }}</div>
+		<div class="post-contents">{{ item.contents }}</div>
+		<div class="post-time">{{ item.createdAt }}</div>
+	</li>
+</template>
+
+<script setup>
+defineProps({
+	item: {
+		type: Object,
+		required: true,
+	},
+});
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,7 +1,29 @@
 <template>
-	<h1>Main</h1>
+	<div class="main list-container contents">
+		<h1 class="page-header">Today I Learned</h1>
+		<ul>
+			<PostListItem
+				v-for="(postItem, index) in postItems"
+				:key="index"
+				:item="postItem"
+			/>
+		</ul>
+	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+import { fetchPosts } from '@/api/index';
+import PostListItem from '@/components/posts/PostListItem.vue';
+
+const postItems = ref([]);
+const fetchData = async () => {
+	const { data } = await fetchPosts();
+	postItems.value = data.posts;
+	console.log(data);
+};
+
+fetchData();
+</script>
 
 <style lang="scss" scoped></style>
